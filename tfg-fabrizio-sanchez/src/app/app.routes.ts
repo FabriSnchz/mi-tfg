1165ca910bd47c2c566c06c0bad3a4592bb421a2
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2025-01-09 12:20:11 +0100

    Added routing

--- tfg-fabrizio-sanchez/src/app/app.routes.ts
+++ tfg-fabrizio-sanchez/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { MiInventarioComponent } from './mi-inventario/mi-inventario.component';
+import { JuegosComponent } from './juegos/juegos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'mi-inventario', component: MiInventarioComponent},
+  {path: 'juegos', component: JuegosComponent},
+  {path: 'contacto', component: ContactoComponent},
+];
