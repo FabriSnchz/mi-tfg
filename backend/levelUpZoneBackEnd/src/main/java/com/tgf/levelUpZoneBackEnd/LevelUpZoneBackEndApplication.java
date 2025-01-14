80f3419854ec5e3204f8a272b7df016c7c7a0050
FabrizioEEP <145449016+FabrizioEEP@users.noreply.github.com>
2025-01-14 10:30:00 +0100

    Created backend project

---
+++ backend/levelUpZoneBackEnd/src/main/java/com/tgf/levelUpZoneBackEnd/LevelUpZoneBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tgf.levelUpZoneBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpZoneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpZoneBackEndApplication.class, args);
+	}
+
+}
---
+++ backend/levelUpZoneBackEnd/src/test/java/com/tgf/levelUpZoneBackEnd/LevelUpZoneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgf.levelUpZoneBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpZoneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
