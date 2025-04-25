295f9552da607ef14aa210744de9e9971aab2a49
FabriSnchz <fabri_k-po20@hotmail.com>
2025-04-25 20:16:28 +0200

    feat: added dark/light mode toggle and user dropdown menu

--- frontend/levelUpZoneFrontEnd/src/app/app.component.ts
+++ frontend/levelUpZoneFrontEnd/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { APP_ID, Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     providers: [
@@ -14,5 +15,17 @@ import { RouterOutlet } from '@angular/router';
     ]
 })
 export class AppComponent {
+  isDarkTheme = false; // Puedes controlar esto con un botón de toggle
   title = 'level-up-zone';
+
+  toggleThemeFromHeader(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+    if (this.isDarkTheme) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-theme');
+    } else {
+      document.body.classList.remove('dark-mode');
+      document.body.classList.add('light-theme');
+    }
+  }
 }
--- frontend/levelUpZoneFrontEnd/src/app/header/header.component.ts
+++ frontend/levelUpZoneFrontEnd/src/app/header/header.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive, MatSlideToggleModule, MatButtonModule, MatMenuModule],
     // RouterLinkActive es una directiva que agrega una clase CSS cuando el enlace está activo
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  @Output() themeToggled = new EventEmitter<void>();
+
+  toggleTheme(): void {
+    this.themeToggled.emit(); // sólo emite el evento
+  }
 }
