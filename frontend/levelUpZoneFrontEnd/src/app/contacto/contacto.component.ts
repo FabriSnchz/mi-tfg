5b8ac4f88c82dad438eb76beb8cfd009104e9e47
FabriSnchz <fabri_k-po20@hotmail.com>
2025-04-27 19:18:15 +0200

    update(contacto): better organized contact cards

--- frontend/levelUpZoneFrontEnd/src/app/contacto/contacto.component.ts
+++ frontend/levelUpZoneFrontEnd/src/app/contacto/contacto.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-contacto',
     standalone: true,
-    imports: [],
+    imports: [MatIconModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './contacto.component.html',
     styleUrl: './contacto.component.scss'
 })
