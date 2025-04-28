c7ff0a1673e288c7f9dc88ecdc3fae21bfff052b
FabriSnchz <fabri_k-po20@hotmail.com>
2025-04-29 01:57:33 +0200

    feat(contacto): add FAQ section and improve contact cards
    - Added a Frequently Asked Questions (FAQ) section to the contact page using Exapsion Panel from Angular Material.
    - Improved the design and functionality of contact cards for a better user experience.

--- frontend/levelUpZoneFrontEnd/src/app/contacto/contacto.component.ts
+++ frontend/levelUpZoneFrontEnd/src/app/contacto/contacto.component.ts
@@ -1,14 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'
 
 @Component({
     selector: 'app-contacto',
     standalone: true,
-    imports: [MatIconModule],
+    imports: [MatIconModule, MatExpansionModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './contacto.component.html',
     styleUrl: './contacto.component.scss'
 })
 export class ContactoComponent {
-
 }
