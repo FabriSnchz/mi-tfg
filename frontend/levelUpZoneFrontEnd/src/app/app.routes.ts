3b6e3a72d0d37f7ea5c9a245c03b8a9aa5a29c17
FabriSnchz <fabri_k-po20@hotmail.com>
2025-04-29 02:13:39 +0200

    feat(asd): create sandbox component for code prototyping

--- frontend/levelUpZoneFrontEnd/src/app/app.routes.ts
+++ frontend/levelUpZoneFrontEnd/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { JuegosComponent } from './juegos/juegos.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CardsComponent } from './cards/cards.component';
+import { AsdComponent } from './asd/asd.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/inicio', pathMatch: 'full', title: 'Inicio'},
   {path: 'cards', component: CardsComponent, title: 'Inicio'},
+  {path: 'asd', component: AsdComponent, title: 'Asd'},
   {path: 'inicio', component: InicioComponent, title: 'Inicio'},
   {path: 'mi-inventario', component: MiInventarioComponent, title: 'Mi Inventario'},
   {path: 'juegos', component: JuegosComponent, title: 'Juegos'},
---
+++ frontend/levelUpZoneFrontEnd/src/app/asd/asd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-asd',
+  templateUrl: './asd.component.html',
+  styleUrls: ['./asd.component.scss'],
+  imports: [CommonModule],
+})
+export class AsdComponent {
+
+}
