b928a1c9a4df3ed26654ccee0838629685771f44
FabriSnchz <fabri_k-po20@hotmail.com>
2025-05-05 17:58:51 +0200

    style(home): improve visual layout and loading animations

--- frontend/levelUpZoneFrontEnd/src/app/home/home.component.ts
+++ frontend/levelUpZoneFrontEnd/src/app/home/home.component.ts
@@ -2,10 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
-
-
-
-
 @Component({
     selector: 'app-home',
     standalone: true,
@@ -17,37 +13,37 @@ export class HomeComponent {
   slides = [
     {
       image: 'https://cdn.pixabay.com/photo/2024/05/04/01/37/retro-8738140_1280.jpg',
-      text: 'Texto de la imagen 1'
+      text: 'Pixel Noir: El nuevo RPG de misterio que combina arte retro y narrativa cinematográfica'
 
     },
     {
       image: 'https://cdn.pixabay.com/photo/2024/09/30/03/56/retro-9084614_1280.png',
-      text: 'Texto de la imagen 2'
+      text: 'Regreso al 8-Bit: Nintendo revive la nostalgia con una edición especial de la NES'
     },
     {
       image: 'https://cdn.pixabay.com/photo/2022/12/14/19/32/death-knight-7656097_1280.jpg',
-      text: 'Texto de la imagen 3'
+      text: 'El Renacer del Guerrero: Se filtran imágenes del nuevo villano de Dark Realms IV'
     },
     {
       image: 'https://cdn.pixabay.com/photo/2020/01/10/14/18/war-4755403_1280.jpg',
-      text: 'Texto de la imagen 4'
+      text: 'Los jueguetes del videojuego más esperado del año: ¡Ya están aquí!'
     },
     {
       image: 'https://cdn.pixabay.com/photo/2014/10/04/06/45/war-472611_1280.jpg',
-      text: 'Texto de la imagen 5'
+      text: 'Los videojuegos más esperados de 2024: ¡No te los pierdas!'
     },
     {
       image: 'https://cdn.pixabay.com/photo/2018/06/27/10/06/game-3501200_1280.jpg',
-      text: 'Texto de la imagen 6'
+      text: 'Las consolas antiguas que están volviendo a la vida: ¡Descúbrelas!'
     }
   ];
 
   customOptions = {
-    items: 1, // o 4, como prefieras
+    items: 1,
     loop: true,
     center: true,
     margin: 10,
-    nav: true,
+    nav: false,
     dots: true,
     autoplay: true,
     autoplayTimeout: 3000,
